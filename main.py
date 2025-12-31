642639f6392ba7d64c3b8233dfbd84df045ed711
martyna <codingbee77@tlen.pl>
2025-12-31 17:17:46 +0100

    Implement log processing functionality and add type hinting

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import re
 from concurrent.futures import ProcessPoolExecutor
+from pathlib import Path
 
 
-def log_reader(file_path):
+def log_reader(file_path: Path) -> str:
     """Generator to read a file line by line."""
     with open(file_path, "r") as f:
         for line in f:
             yield line.strip()
 
 
-def clean_log(line):
+def clean_log(line: str) -> str | None:
     """Check if the word 'ERROR' is in the line and if so, returns the line, otherwise None."""
     if m := re.search(r"ERROR", line, re.IGNORECASE):
         return line.upper()
     return None
 
 
-def process_logs():
-    pass
+def process_logs(file_path: Path):
+    """Process logs in parallel and print cleaned lines."""
+
+    reader = log_reader(file_path)
+
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(clean_log, reader)
+
+    # Filter out None results and print the errors
+    for entry in results:
+        if entry:
+            print(f"Found: {entry}")
 
 
 if __name__ == "__main__":
