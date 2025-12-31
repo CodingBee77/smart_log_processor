837812b2f9ddf88726597e964d1e0b389db16cb8
martyna <codingbee77@tlen.pl>
2025-12-31 17:32:33 +0100

    Update log_reader to use mock data and measure performance while running the script

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import re
+import time
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
+from typing import Any, Generator
 
 
-def log_reader(file_path: Path) -> str:
+def log_reader(file_path: Path) -> Generator[str, Any, None]:
     """Generator to read a file line by line."""
-    with open(file_path, "r") as f:
-        for line in f:
-            yield line.strip()
+    # with open(file_path, "r") as f:
+    #     for line in f:
+    #         yield line.strip()
+
+    mock_data = [
+        "Info: system ok",
+        "Error: disk full",
+        "Debug: trace",
+        "ERROR: auth fail",
+    ] * 10000
+    for line in mock_data:
+        yield line
 
 
 def clean_log(line: str) -> str | None:
@@ -23,13 +34,12 @@ def process_logs(file_path: Path):
     reader = log_reader(file_path)
 
     with ProcessPoolExecutor() as executor:
-        results = executor.map(clean_log, reader)
-
-    # Filter out None results and print the errors
-    for entry in results:
-        if entry:
-            print(f"Found: {entry}")
+        results = executor.map(clean_log, reader, chunksize=500)
+        error_count = sum(1 for entry in results if entry)
+        print(f"Total Errors found: {error_count}")
 
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     process_logs("large_system.log")
+    print(f"Finished in {time.perf_counter() - start:.4f} seconds.")
